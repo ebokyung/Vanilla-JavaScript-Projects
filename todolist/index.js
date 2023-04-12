3272619d075854b25d444634e392cd2c0feb6ac0
bokyung_lee <dlqhrud303@naver.com>
2023-04-12 14:33:26 +0900

    리스트 추가하기

---
+++ todolist/index.js
@@ -0,0 +1,19 @@
+const btnDel = document.querySelectorAll(".btn-del")
+const btnAdd = document.querySelector(".btn-add")
+const btnClear = document.querySelector(".btn-clear")
+const listContainer = document.querySelector('.list-container');
+const inp = document.querySelector('#inpTodo')
+
+btnAdd.addEventListener('click', () => {
+    const li = document.createElement('li')
+    let input = inp.value;
+    let idNum = listContainer.childElementCount + 1;
+    let item = `<li class="list-item">
+                    <label for="chckList${idNum}"></label>
+                    <input id="chckList${idNum}" type="checkbox">
+                    <p>${input}</p>
+                    <button class="btn-del" type="button">삭제</button>
+                </li>`
+    li.innerHTML = item
+    listContainer.append(li)
+})
